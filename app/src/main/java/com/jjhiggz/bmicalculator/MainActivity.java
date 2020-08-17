017554dbf24b1cc9629ad0981f23595be3e62001
jjhiggz <jonathan.higger@gmail.com>
2020-08-17 14:46:50 -0600

    all id's connected

--- app/src/main/java/com/jjhiggz/bmicalculator/MainActivity.java
+++ app/src/main/java/com/jjhiggz/bmicalculator/MainActivity.java
@@ -2,12 +2,48 @@ package com.jjhiggz.bmicalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView resultText = findViewById(R.id.text_view_result);
+
+        EditText yourAge = findViewById( R.id.edit_text_age );
+        EditText yourFeet = findViewById(R.id.edit_text_feet);
+        EditText yourInches = findViewById(R.id.edit_text_inches);
+        EditText yourWeight = findViewById(R.id.edit_text_weight);
+
+        RadioButton maleButton = findViewById(R.id.radio_button_male);
+        RadioButton femaleButton = findViewById(R.id.radio_button_female);
+
+        Button calculateButton = findViewById(R.id.calculate_button);
+
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        resultText.setText("I can change my text view using programming code");
+
+
+
     }
+
+
+
 }
