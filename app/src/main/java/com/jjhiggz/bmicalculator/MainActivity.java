f4f94eba1a67abcb5ccc6cf47c98220de58fe55a
jjhiggz <jonathan.higger@gmail.com>
2020-08-18 14:02:25 -0600

    bmi calculator working

--- app/src/main/java/com/jjhiggz/bmicalculator/MainActivity.java
+++ app/src/main/java/com/jjhiggz/bmicalculator/MainActivity.java
@@ -11,38 +11,79 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    // class variables are also called "fields"
+    private TextView resultText;
+    private EditText yourAge;
+    private EditText yourFeet;
+    private EditText yourInches;
+    private EditText yourWeight;
+    private RadioButton maleButton;
+    private RadioButton femaleButton;
+    private Button calculateButton;
 
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+        setupButtonClickListener();
+    }
 
-        TextView resultText = findViewById(R.id.text_view_result);
 
-        EditText yourAge = findViewById( R.id.edit_text_age );
-        EditText yourFeet = findViewById(R.id.edit_text_feet);
-        EditText yourInches = findViewById(R.id.edit_text_inches);
-        EditText yourWeight = findViewById(R.id.edit_text_weight);
+    private void findViews(){
 
-        RadioButton maleButton = findViewById(R.id.radio_button_male);
-        RadioButton femaleButton = findViewById(R.id.radio_button_female);
+        resultText = findViewById(R.id.text_view_result);
+        yourAge = findViewById( R.id.edit_text_age );
+        yourFeet = findViewById(R.id.edit_text_feet);
+        yourInches = findViewById(R.id.edit_text_inches);
+        yourWeight = findViewById(R.id.edit_text_weight);
 
-        Button calculateButton = findViewById(R.id.calculate_button);
+        maleButton = findViewById(R.id.radio_button_male);
+        femaleButton = findViewById(R.id.radio_button_female);
+        calculateButton = findViewById(R.id.calculate_button);
 
+    }
+
+
+    private void setupButtonClickListener() {
         calculateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Cool I made this thing popo up by clicking a button", Toast.LENGTH_LONG).show();
+                calculateBMI();
             }
         });
+    }
 
-        resultText.setText("I can change my text view using programming code");
-
-
+    private void calculateBMI() {
+        int age = parse(yourAge.getText().toString());
+        int feet = parse(yourFeet.getText().toString());
+        int inches = parse(yourInches.getText().toString());
+        float weight = getPounds(
+               parse(
+                       yourWeight.getText().toString()
+               )
+        );
+        float heightInMeters = getHeightInMeters(inches,feet);
+        resultText.setText( String.valueOf(weight / (heightInMeters * heightInMeters)) );
 
     }
+
+    private int parse( String string){
+        return Integer.parseInt(string);
+    }
+
+    private float getPounds( int kg ){
+
+        return kg / (float)2.20462;
+    };
+    private float getHeightInMeters( int inches , int feet){
+        int totalInches = inches + feet * 12;
+        return totalInches* (float)0.0254;
+
+    }
+
 
 
 
