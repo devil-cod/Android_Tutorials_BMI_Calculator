778f56f2ced8fa20bd5fee9e725b4e9791d4d1f9
Ankesh Ojha <ankeshojha@197NODMBT2798.local>
2022-02-21 11:32:41 +0530

    jj

---
+++ pus.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*
+public class pus {
+
+    public static void main(String[] args)
+    {
+        System.out.println(" Ankesh ojha"
+                );
+
+    }
+}
